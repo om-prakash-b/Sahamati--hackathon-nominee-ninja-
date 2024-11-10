ba049e510e2f3e621b67ee6e6e81bed44ae40432
om-prakash-b <mailtoomprakash.b@gmail.com>
2024-11-10 15:13:34 +0530

    Add files via upload

---
+++ sahamati acess tokens database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+module.exports = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+};
